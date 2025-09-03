74dfb5c6c8a2c37cf11ccf70f1ffba086b7fcd1c
jtn9701 <jtnguy01@gmail.com>
2025-09-03 01:31:27 -0500

    Start on miniboss script

--- game/scripts/miniboss.js
+++ game/scripts/miniboss.js
@@ -1,16 +1,18 @@
-ap.src += location.search;
-forest.href += location.search; //Change
-var queryString = new URLSearchParams(location.search);
+function decrementMinibossHP() {
+    var minibossHP = 8;
 
-function attack(){
-    minAPNeeded = 8;
-    let isMinibossDead = false;
-    if (ap < minAPNeeded){
-        isMinibossDead = true;
-        battleText.innerHTML = "You defeated the goon"
-        options.innerHTML = `<a href='forest.html?${queryString}'><button>Go back</button></a>` //Change
+    if (minibossHP > 0) {
+        minibossHP--;
+        hpText.innerHTML = `The enemy has ${minibossHP} hp. Click on him to attack.`;
+    } else{
+        battleText.innerHTML = "You defeated the catnapper's goon! You can now proceed to the next area.";
+        hpText.innerHTML = "";
+        
+        const overworld1button = document.getElementById("overworld");
+        overworld1button.style.visibility = "hidden";
+
+        const overworld2button = document.getElementById("overrworld2");
+        overworld2button.style.visibility = "visible";
     }
-    else {
-        battleText.innerHTML = "You don't have enough AP to defeat the goon. Find more cats."
-    }
+
 }
